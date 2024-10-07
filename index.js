361e01f2a341b775118e936b24d6252c8d68cf40
craigkibera <craigituro@gmail.com>
2024-10-07 14:41:46 +0300

    events

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  document.getElementById('button').addEventListener
+  ('click',clickAlert)
+  function clickAlert() {
+    return alert('I was clicked!');
+  }
 }
+
+
+
+// document.getElementById('button').addEventListener
+// ('click',() => alert('I was clicked'))
